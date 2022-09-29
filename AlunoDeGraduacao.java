d35cace92dd471786ff8a8ff05545916c72ace2f
Itamara <Itamara_1245@hotmail.com>
2022-09-29 17:34:22 -0300

    heranca(apostila): fim da listagem 1.3.3.3

--- AlunoDeGraduacao.java
+++ AlunoDeGraduacao.java
@@ -2,7 +2,6 @@ public class AlunoDeGraduacao
 extends Pessoa{
     private double nota1, nota2, notaFinal;
 }
-    
     //getters / seters
 
     https://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@github.com/ItamaraCristina/Fatec-IPI-POO-Heranca.git
